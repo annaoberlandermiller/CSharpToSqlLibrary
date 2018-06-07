2206614db57c914c9fabbcf0f171ffb6ac9ec90f
Anna Miller <annaoberlandermiller@gmail.com>
2018-06-07 11:02:15 -0400

    inital commit

---
+++ SqlLibrary/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary { //we added this from another solution. We had to change the namespace to SqlLibrary so we could use this.
+					   //We could use a using statement instead
+					   //This is a COPY, or a CLONE of this class.  Any changes we make will not affect the origin class. 
+{
+    public class User {//mark the class as public if the data is going to public
+    
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+		public string Phone { get; set; }
+		public string Email { get; set; }
+		public bool IsReviewer { get; set; }
+		public bool IsAdmin { get; set; }
+		public bool Active { get; set; }
+
+		public User() { }
+	}
+}
---
+++ SqlLibrary/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary {
+	public class UsersController {  //make sure to mark as PUBLIC.  This is another project.  
+									//it's not good practice to have a collection return a method. If there's no entries it won't return anything anyway
+		public IEnumerable<User> List() {
+			return new List<User>();
+		}
+		public User Get(int id) {
+			return null;
+		}
+		public bool Create(User user) {
+			return false;
+		}
+		public bool Change(User user) {
+			return false;
+		}
+		public bool Remove(User user) {
+			return false;
+		}
+	}
+}
